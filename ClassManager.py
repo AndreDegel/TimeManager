1838e98dfc8e4161761c898f8d3232bdffc59f1b
Andre <dego89@gmx.net>
2015-04-22 15:51:53 -0500

    Working test data with output.

--- ClassManager.py
+++ ClassManager.py
@@ -12,23 +12,48 @@ cxn = sqlite3.connect('GymDB')
 cur = cxn.cursor()
 # start a try-except block to handle SQL-Exceptions
     # create the table and fill it with data
-'''
+
 cur.execute('CREATE TABLE Instructor(id INTEGER PRIMARY KEY, firstName VARCHAR(50), lastName VARCHAR(50), address VARCHAR(50))')
 cur.execute('CREATE TABLE Class(code VARCHAR(8) PRIMARY KEY, description VARCHAR(50), price FLOAT, instructorID INTEGER REFERENCES Instructor(id))')
 cur.execute('CREATE TABLE Schedule(code VARCHAR(8) REFERENCES Class(code), Day TEXT, time TEXT)')
 cur.execute('CREATE TABLE Student(id INTEGER PRIMARY KEY, firstName VARCHAR(50), lastName VARCHAR(50), address VARCHAR(50), amountDue FLOAT)')
 cur.execute('CREATE TABLE ClassStudent(code VARCHAR(8) REFERENCES Class(code), sequence INTEGER, studentID INTEGER REFERENCES Student(id))')
-'''
+
 
 cur.execute('INSERT INTO Instructor VALUES(NULL, "Bob", "Smith", "1234 Street")')
 cur.execute('INSERT INTO Class VALUES("Weight", "Lifting weights", 250.00, 1)')
 cur.execute('INSERT INTO Schedule VALUES("Weight", "Monday", "19:00")')
 cur.execute('INSERT INTO ClassStudent VALUES("Weight", 1, 1)')
-cur.execute('INSERT INTO Student VALUES(1, "Jane", "Smith", "1500 Tree Rd.", 0)')
-'''
+cur.execute('INSERT INTO Student VALUES(NULL, "Jane", "Smith", "1500 Tree Rd.", 0)')
+
+#  To retrieve data after executing a SELECT statement, you can either treat the cursor as an iterator,
+#  call the cursor’s fetchone() method to retrieve a single matching row, or call fetchall() to get a list of the matching rows.
 cur.execute('SELECT * FROM Instructor')
+for eachUser in cur.fetchall():
+    print("Successfully created the users table.")
+    print(eachUser)
+
 cur.execute('SELECT * FROM Class')
+for eachUser in cur.fetchall():
+    print("Successfully created the users table.")
+    print(eachUser)
+
 cur.execute('SELECT * FROM Schedule')
+for eachUser in cur.fetchall():
+    print("Successfully created the users table.")
+    print(eachUser)
+
 cur.execute('SELECT * FROM ClassStudent')
-cur.execute('SELECT * FROM Student')'''
+for eachUser in cur.fetchall():
+    print("Successfully created the users table.")
+    print(eachUser)
+
+cur.execute('SELECT * FROM Student')
+for eachUser in cur.fetchall():
+    print("Successfully created the users table.")
+    print(eachUser)
 # handle an sql exception if the table already exists
+
+cur.close()
+cxn.commit()
+cxn.close()
