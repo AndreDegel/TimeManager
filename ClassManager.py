b2709e1f19ccd43e071b6e47ababffe8d366901d
Akeea <akeeawhite@gmail.com>
2015-04-29 09:38:28 -0500

    Added delete Instructor button, window, and code.

--- ClassManager.py
+++ ClassManager.py
@@ -89,15 +89,16 @@ class MainWindow():
             #Declares and defines labels, buttons, and textboxes
             self.btnAddInstructor = Button(self.master, text="Add Instructor", width=13, command=self.addInstructor)
             self.btnAddStudent = Button(self.master, text="Add Student", width=13, command=self.addStudent)
-
+            self.btnDeleteInstructor = Button(self.master, text="Delete Instructor", width=13, command=self.deleteInstructor)
             self.btnSearch = Button(self.master, text="Search", width=13, command=self.search)
             self.btnQuit = Button(self.master, text="Quit", width=13, command=self.quit)
 
             #Aligns all of the labels, buttons, and textboxes in grid form
             self.btnAddInstructor.grid(row=1, column=1, sticky=W)
             self.btnAddStudent.grid(row=2, column=1, sticky=W)
-            self.btnSearch.grid(row=3, column=1, sticky=W)
-            self.btnQuit.grid(row=4, column=1)
+            self.btnDeleteInstructor.grid(row=3, column=1, sticky=W)
+            self.btnSearch.grid(row=4, column=1, sticky=W)
+            self.btnQuit.grid(row=5, column=1)
 
     #Function for quit button to close window
     def quit(self):
@@ -114,6 +115,12 @@ class MainWindow():
         #Sets the addStudent class to root2 so it is displayed when Add Instructor button is clicked
         root2 = Toplevel(self.master)
         AddStudent(root2)
+
+    #Function for displaying the delete instructor window
+    def deleteInstructor(self):
+        #Sets the addInstructor class to root2 so it is displayed when Add Instructor button is clicked
+        root2 = Toplevel(self.master)
+        DeleteInstructor(root2)
 
     #Function for displaying the searching window
     def search(self):
@@ -248,6 +255,66 @@ class AddStudent:
 
         except sqlite3.IntegrityError:
             messagebox.showwarning("New Student could not be added")
+
+        finally:
+            #cur.close()
+            cxn.commit()
+            #cxn.close()
+
+
+#Class for deleting Instructors
+class DeleteInstructor:
+
+    #myMainWindowClass = MainWindow(empName)
+    def __init__(self, master):
+
+        #creates master window for calculation window, changes title, and sets size
+        self.master = master
+        self.master.title("Delete Instructor")
+        self.master.geometry("250x140")
+
+        self.firstName = StringVar()
+        self.lastName = StringVar()
+        self.address = StringVar()
+
+        #Creates labels for outputting the calculations
+        self.lblFirst = Label(self.master, text="First Name: ")
+        txtBoxEmployeeFirstName = Entry(self.master, textvariable=self.firstName)
+        self.lblLast = Label(self.master, text="Last Name: ")
+        txtBoxEmployeeLastName = Entry(self.master, textvariable=self.lastName)
+        self.lblAddress = Label(self.master, text="Address: ")
+        txtBoxEmployeeAddress = Entry(self.master, textvariable=self.address)
+
+        #Close Button
+        self.btnClose = Button(self.master, text="Close", width=8, command=self.quit)
+
+        #Aligns button in grid
+        self.btnClose.grid(row=5, column=2)
+
+        #Aligns the labels using the grid
+        self.lblFirst.grid(row=1, column=1, sticky=W)
+        txtBoxEmployeeFirstName.grid(row=1, column=2, sticky=E)
+        self.lblLast.grid(row=2, column=1, sticky=W)
+        txtBoxEmployeeLastName.grid(row=2, column=2)
+        self.lblAddress.grid(row=3, column=1, sticky=W)
+        txtBoxEmployeeAddress.grid(row=3, column=2)
+        self.btnDelete = Button(self.master, text="Delete", width=8, command=self.delete)
+        self.btnDelete.grid(row=4, column=2)
+
+    def quit(self):
+        self.master.destroy()
+
+    def delete(self):
+        # Delete instructors to the database
+        first = self.firstName.get()
+        last = self.lastName.get()
+        address = self.address.get()
+        try:
+            cur.execute('DELETE FROM Instructor WHERE address= ? ')
+            messagebox.showwarning("Instructor Deleted", " Instructor successfully deleted")
+
+        except sqlite3.IntegrityError:
+            messagebox.showwarning("Instructor could not be deleted")
 
         finally:
             #cur.close()
