0d9e6a18bc87d24df33cb1d66a4f6f226c4d5259
Andre <dego89@gmx.net>
2015-05-13 17:26:37 -0500

    Added Student course code and corresponding search, cleaned other searches.

---
+++ AddStudentCourse.py
@@ -0,0 +1,100 @@
+__author__ = 'Andre'
+
+from tkinter import messagebox
+from tkinter import ttk
+from tkinter import *
+import sqlite3
+
+# create a connection to the newly made database
+cxn = sqlite3.connect('GymDB')
+# initialize a cursor object to run execute commands on the connected database.
+cur = cxn.cursor()
+# TODO: Add returning for all instructors, and show instructors students or schedule
+# TODO: which students in which class(by day)
+# TODO: handle errors of one student registering for the same class
+#Class for deleting Instructors
+class AddStudentCourse:
+
+    #myMainWindowClass = MainWindow(empName)
+    def __init__(self, master):
+
+        #creates master window for calculation window, changes title, and sets size
+        self.master = master
+        self.master.title("Register Student")
+        self.master.geometry("220x70")
+
+        self.courseValues = []
+        self.studentValues = []
+        self.studentID = {}
+        self.courseID = {}
+        self.sequenceCount = 1
+
+       # get the values from the database for the combo boxes
+        try:
+            cur.execute("SELECT description, code FROM Class")
+            for course in cur.fetchall():
+                #have to parse into string and access placeholder here because fetch... returns tuple
+                stringCourse = str(course[0])
+                self.courseValues.append(stringCourse)
+                self.courseID[stringCourse] = course[1]
+
+            cur.execute("SELECT firstName, lastName, id  FROM Student")
+            for student in cur.fetchall():
+                #have to parse into string and access placeholder here because fetch... returns tuple
+                stringCourse = str(student[0]) + " " + str(student[1])
+                self.studentValues.append(stringCourse)
+                self.studentID[stringCourse] = student[2]
+        except sqlite3.Connection:
+            messagebox.showerror("Could not connect to the database")
+
+        # create a combobox to store in the courses
+        # https://docs.python.org/3.1/library/tkinter.ttk.html#combobox
+        self.courseCombo = ttk.Combobox(self.master, values=self.courseValues, state="readonly")
+        # set the current selection to the first item
+        self.courseCombo.current(0)
+        self.courseCombo.grid(row=3, column=2)
+
+
+        self.studentCombo = ttk.Combobox(self.master, values=self.studentValues, state="readonly")
+        self.studentCombo.current(0)
+        self.studentCombo.grid(row=1, column=2)
+        #Creates labels for outputting the calculations
+        self.lblStudent = Label(self.master, text="Student: ")
+
+        #Close Button
+        self.btnClose = Button(self.master, text="Close", width=8, command=self.quit)
+
+        #Aligns button in grid
+        self.btnClose.grid(row=2, column=2)
+
+        #Aligns the labels using the grid
+        self.lblStudent.grid(row=1, column=1, sticky=W)
+
+        self.btnRegister = Button(self.master, text="Register", width=8, command=self.register)
+        self.btnRegister.grid(row=2, column=1)
+
+    def quit(self):
+        self.master.destroy()
+
+    def register(self):
+        # get the current combobox values
+        student = self.studentCombo.get()
+        course = self.courseCombo.get()
+        courseID = self.courseID.get(course)
+        studentid = self.studentID.get(student)
+        sequence = 0
+        # no need to validate because cannot be null and not different than
+        # whats in the database
+        # but need to figure out sequence
+        try:
+            cur.execute('SELECT COUNT(sequence) as num FROM ClassStudent WHERE studentID = ?', (studentid,))
+            sequence = self.sequenceCount + cur.fetchone()[0]
+
+            cur.execute('Insert INTO ClassStudent VALUES (?, ?, ?)', (courseID, sequence, studentid))
+            messagebox.showwarning("Class Registered", " Class successfully registered")
+
+        except sqlite3.Error as e:
+            messagebox.showwarning(e)
+
+        finally:
+            cxn.commit()
--- Search.py
+++ Search.py
@@ -8,6 +8,7 @@ import sqlite3
 cxn = sqlite3.connect('GymDB')
 # initialize a cursor object to run execute commands on the connected database.
 cur = cxn.cursor()
+#TODO: Have students for classes by day/not all days.
 
 #Class for searching for instructors, times, or classes
 class Search:
@@ -17,7 +18,7 @@ class Search:
         #creates master window for calculation window, changes title, and sets size
         self.master = master
         self.master.title("Search")
-        self.master.geometry("250x200")
+        self.master.geometry("300x250")
 
         #Sets the string variable and checkBox variables
         self.userSearchInput = StringVar()
@@ -25,12 +26,14 @@ class Search:
         self.chkVar2 = IntVar()
         self.chkVar3 = IntVar()
         self.chkVar4 = IntVar()
+        self.chkVar5 = IntVar()
 
         #Widgets
         self.chkBtnInstructorName = Checkbutton(self.master, text="Instructor Last Name", variable=self.chkVar1, justify=LEFT)
         self.chkBtnClassName = Checkbutton(self.master, text="Class Name", variable=self.chkVar2, justify=LEFT)
         self.chkBtnClassDay = Checkbutton(self.master, text="Class Day", variable=self.chkVar3, justify=LEFT)
         self.chkBtnAllClassStudent = Checkbutton(self.master, text="Student class registers", variable=self.chkVar4, justify=LEFT)
+        self.chkBtnGetStudentTime = Checkbutton(self.master, text="Student time schedule", variable=self.chkVar5, justify=LEFT)
         self.lblSearchInformation = Label(self.master, text="Search For Instructor, Classes, or schedules")
         self.lblSearch = Label(self.master, text="Search: ")
         self.txtBoxSearch = Entry(self.master, textvariable=self.userSearchInput)
@@ -46,10 +49,11 @@ class Search:
         self.chkBtnClassName.grid(row=3, columnspan=1, sticky=W)
         self.chkBtnClassDay.grid(row=4, columnspan=1, sticky=W)
         self.chkBtnAllClassStudent.grid(row=5, columnspan=1, sticky=W)
-        self.lblSearch.grid(row=6, columnspan=1, sticky=W)
-        self.txtBoxSearch.grid(row=7, columnspan=1, sticky=W)
-        self.btnSearch.grid(row=8, columnspan=1, sticky=W)
-        self.btnClose.grid(row=9, columnspan=1, sticky=W)
+        self.chkBtnGetStudentTime.grid(row=6, columnspan=1, sticky=W)
+        self.lblSearch.grid(row=7, columnspan=1, sticky=W)
+        self.txtBoxSearch.grid(row=8, columnspan=1, sticky=W)
+        self.btnSearch.grid(row=9, columnspan=1, sticky=W)
+        self.btnClose.grid(row=10, columnspan=1, sticky=W)
 
     #Searh Query Function
     def searchQuery(self):
@@ -71,11 +75,23 @@ class Search:
                     messagebox._show("Query Results", msg)
 
                 elif(self.chkVar2.get()):
-                    cur.execute("SELECT * FROM Class WHERE code LIKE ?", (self.userSearchCriteria,))
-                    messagebox.showwarning("Query Results", cur.fetchall())
+                    classlist = []
+                    cur.execute("SELECT code, description, price, lastName FROM Class, Instructor WHERE Class.instructorID = Instructor.id "
+                                "AND code LIKE ?", (self.userSearchCriteria,))
+                    for info in cur.fetchall():
+                        classes = "Code: " + str(info[0]) + " Description: " + info[1] + " Price: " + info[2] + " Instructor: " + info[3]
+                        classlist.append(classes)
+                    msg = "\n".join(classlist)
+                    messagebox._show("Query Results", msg)
 
                 elif(self.chkVar3.get()):
+                    scheduleList = []
                     cur.execute("SELECT * FROM Schedule WHERE Day LIKE ?", (self.userSearchCriteria,))
+                    for info in cur.fetchall():
+                        schedule = "Code: " + str(info[0]) + " Day: " + info[1] + " Time: " + info[2]
+                        scheduleList.append(schedule)
+                    msg = "\n".join(scheduleList)
+                    messagebox._show("Query Results", msg)
                     messagebox.showwarning("Query Results", cur.fetchall())
 
                 elif(self.chkVar4.get()):
@@ -85,6 +101,22 @@ class Search:
                         instructor = "Code: " + str(info[0]) + " Sequence: " + str(info[1]) + " Student ID:" + str(info[2]) +"\n"
                         register.append(instructor)
                     msg = "\n".join(register)
+                    messagebox._show("Query Results", msg)
+
+                elif(self.chkVar5.get()):
+                    studentSchedule = []
+                    cur.execute("SELECT Student.firstName, Student.lastName, description, Day, time, "
+                                "Instructor.firstName, Instructor.lastName "
+                                "FROM ClassStudent, Class, Schedule, Student, Instructor "
+                                "WHERE Instructor.id = Class.instructorID AND Student.id = ClassStudent.studentID "
+                                "AND Class.code = ClassStudent.code AND Class.code = Schedule.code "
+                                "AND Student.lastName = ?", (self.userSearchCriteria,))
+                    for info in cur.fetchall():
+                        courseTimes = "Student: " + str(info[0]) + " " + str(info[1]) + "Course: " + str(info[2]) \
+                                      + " Day: " + str(info[3]) + " Time:" + str(info[4]) +\
+                                     " Instructor: " + str(info[5]) + " " + str(info[6] +"\n")
+                        studentSchedule.append(courseTimes)
+                    msg = "\n".join(studentSchedule)
                     messagebox._show("Query Results", msg)
 
             except sqlite3.IntegrityError:
